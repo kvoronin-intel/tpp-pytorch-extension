b2fd1fdac02be9ee2cacbc1330f5f17b0d28fe61
kvoronin-intel <kirill.voronin@intel.com>
2022-05-26 17:15:41 -0700

    Modified the conv test to work correctly with padding (non-trivial tensor maniuplations involved to zero rims where necessary)

--- src/tpp_pytorch_extension/resnet/my_run_conv_ext_test_clone.sh
+++ src/tpp_pytorch_extension/resnet/my_run_conv_ext_test_clone.sh
@@ -44,5 +44,5 @@ export OMP_NUM_THREADS=28
 #gdb --args python test_conv_ext.py --test-module ext_tpp #--use-bf16-opt
 #python test_conv_ext.py --test-module ext_tpp #--use-bf16-opt
 #gdb --args python test_conv_ext.py $@ #--use-bf16-opt
-            python test_conv_ext.py $@ #--use-bf16-opt
+            python -u test_conv_ext.py $@ #--use-bf16-opt
 #python test_conv_ext.py --use-bf16-opt --with-perf
--- src/tpp_pytorch_extension/resnet/test_conv_ext.py
+++ src/tpp_pytorch_extension/resnet/test_conv_ext.py
@@ -34,12 +34,15 @@ parser.add_argument('--use-bf16-ref', action="store_true", default=False, dest='
 
 #import pdb
 
+global_counter = 0
+
 #torch.autograd.set_detect_anomaly(True)
 
 def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_bias, padding_mode, opt_dtype, ref_dtype, with_perf, test_module):
     print("debug: run_test_conv called with N H W inc outc K stride padding dilation groups has_bias padding_mode opt_dtype ref_dtype with_perf test_module ",
             N, H, W, inc, outc, K, stride, padding, dilation, groups, has_bias, padding_mode, opt_dtype, ref_dtype, with_perf, test_module)
 
+    global global_counter
     #pcl_cgbp.init_libxsmm() # Not needed?
 
     #inc = 4
@@ -49,6 +52,8 @@ def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_
         if inc % 2 != 0:
             inc = inc + 1
 
+    opt_has_physical_padding = False
+
     torch.manual_seed(0)
     if test_module == 'cnn_tpp':
         print("info: testing TPP module from CNN (pcl_cgbp)")
@@ -56,7 +61,9 @@ def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_
         hardcoded_bc=64
     elif test_module == 'ext_tpp':
         print("info: testing TPP module from extensions (pcl_pytorch_extension)")
+        print("caution: TPP module from extensions only works with physical padding")
         opt_conv = conv_py.DummyConv2dTPP(inc, outc, K, stride, padding, dilation, groups, has_bias, padding_mode, opt_dtype)
+        opt_has_physical_padding = True
         hardcoded_bc=64
     elif test_module == 'handlebased':
         print("info: testing handle-based module")
@@ -69,6 +76,16 @@ def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_
         print("test_module not supported, test_module = ", test_module)
         exit()
 
+    if opt_has_physical_padding != False:
+        #input_hw_padding  = [opt_padding[0], opt_padding[0], opt_padding[1], opt_padding[1]]
+        #output_hw_padding = [opt_padding[2], opt_padding[2], opt_padding[3], opt_padding[3]]
+        input_hw_padding  = [padding, padding, padding, padding]
+        output_hw_padding = [padding, padding, padding, padding]
+        print("input_hw_padding = ",  input_hw_padding)
+        print("output_hw_padding = ", output_hw_padding)
+    else:
+        input_hw_padding  = [0, 0, 0, 0]
+        output_hw_padding = [0, 0, 0, 0]
 
     torch_conv = torch.nn.Conv2d(inc, outc, K, stride, padding, dilation, groups, has_bias, padding_mode, device=None, dtype=ref_dtype)
 
@@ -104,6 +121,9 @@ def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_
         opt_weight_init = weight
     if has_bias:
         opt_bias_init = bias
+
+    if opt_has_physical_padding != False:
+        opt_x_init = torch.nn.functional.pad(opt_x_init, input_hw_padding, mode='constant', value=0.0)
 
     if ref_dtype == torch.bfloat16:
         ref_x_init = x_bf16
@@ -162,6 +182,23 @@ def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_
     y1 = opt_conv(x1) #xp)
     y2 = torch_conv(x2)
 
+    if opt_has_physical_padding != False:
+        y1_unblocked  = y1.unblocked_tensor()
+        print("debug: Zeroing the rim of the output tensor on the Python side")
+        nchw_shape = y1_unblocked.shape
+        y1_zeroed_rim = torch.zeros_like(y1_unblocked)
+        print("debug: nchw shape = ", nchw_shape)
+        outH = nchw_shape[2] - output_hw_padding[0] - output_hw_padding[1]
+        outW = nchw_shape[3] - output_hw_padding[2] - output_hw_padding[3]
+        print("range = ", 'full', ' ', 'full', output_hw_padding[0], outH + output_hw_padding[0], output_hw_padding[2], outW + output_hw_padding[2])
+        y1_zeroed_rim[:,:,output_hw_padding[0]:outH + output_hw_padding[0],output_hw_padding[2]:outW + output_hw_padding[2]] = y1_unblocked[:,:,output_hw_padding[0]:outH + output_hw_padding[0],output_hw_padding[2]:outW + output_hw_padding[2]]
+        #for i in range (40):
+        #    print("ind y1_zeroed_rim[end-ind] = ", i, y1_zeroed_rim.view(-1)[-i].item())
+        opt_y_fp32 = y1_zeroed_rim.to(torch.float)
+    else:
+        opt_y_fp32 = y1.unblocked_tensor().to(torch.float)
+    #opt_y_fp32 = y1.unblocked_tensor().to(torch.float)
+
     #for i in range(10):
     #    print("i y1 y2 = ", i, y1.view(-1)[i].item(), y2.view(-1)[i].item())
     #for i in range(10):
@@ -169,17 +206,79 @@ def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_
 
     z1 = y1.mean()
     z2 = y2.mean()
-    print("z1", z1)
-    print("z2", z2)
-    z1.backward(retain_graph=True)
+
+    if opt_has_physical_padding != False:
+        y1_numel = y1.unblocked_tensor().numel() # if hasattr(y1,unblocked_tensor) else y1.numel() does not work as a check
+        print("z1 for zeroed rim (with account for padding)", y1_zeroed_rim.mean() * y1_numel / y2.numel())
+    else:
+        y1_numel = y1.unblocked_tensor().numel() # if hasattr(y1,unblocked_tensor) else y1.numel() does not work as a check
+        print("z1                         ", z1)
+    print("z2                         ", z2)
+
+    """
+    #temp
+    ref_y_fp32 = y2.to(torch.float)
+    if opt_has_physical_padding != False:
+        ref_y_fp32 = torch.nn.functional.pad(ref_y_fp32, output_hw_padding, mode='constant', value=0.0)
+    shift = output_hw_padding[0] * (W + output_hw_padding[2] + output_hw_padding[3]) + output_hw_padding[2]
+    print("shift = ", shift)
+    for i in range(10):
+        ind = i + shift - 5 if opt_has_physical_padding != False else i
+        print("ind opt_y_fp32 ref_y_fp32 = ", ind, opt_y_fp32.view(-1)[ind].item(), ref_y_fp32.view(-1)[ind].item())
+
+    #np.savetxt('conv_forward_run_' + str(global_counter) + '.txt', opt_y_fp32.contiguous().view(-1).detach().to(torch.float).numpy())
+    global_counter = global_counter + 1
+
+    return
+    exit()
+    """
+
+    if opt_has_physical_padding != False:
+        x1.requires_grad              = False
+        y1._t.retain_grad()
+        opt_conv.weight.requires_grad = False
+        if has_bias:
+            opt_conv.bias.requires_grad   = False
+        z1.backward(retain_graph=True,gradient=torch.tensor(1.*y1_numel / y2.numel(), dtype=torch.float))
+        # zero the rim
+        print("debug: Zeroing the rim of the output tensor on the Python side")
+        nchw_shape = y1.grad.shape
+        y1_grad_zeroed_rim = torch.zeros_like(y1.grad)
+        print("debug: nchw shape = ", nchw_shape)
+        outH = nchw_shape[2] - output_hw_padding[0] - output_hw_padding[1]
+        outW = nchw_shape[3] - output_hw_padding[2] - output_hw_padding[3]
+        print("range = ", 'full', ' ', 'full', output_hw_padding[0], outH + output_hw_padding[0], output_hw_padding[2], outW + output_hw_padding[2])
+        y1_grad_zeroed_rim[:,:,output_hw_padding[0]:outH + output_hw_padding[0],output_hw_padding[2]:outW + output_hw_padding[2]] = y1.grad[:,:,output_hw_padding[0]:outH + output_hw_padding[0],output_hw_padding[2]:outW + output_hw_padding[2]]
+        # now doing the main backward()
+        x1.requires_grad              = True
+        opt_conv.weight.requires_grad = True
+        if has_bias:
+            opt_conv.bias.requires_grad   = True
+        y1._t.backward(gradient=y1_grad_zeroed_rim)
+    else:
+        z1.backward(retain_graph=True)
+
     z2.backward(retain_graph=True)
 
     #for i in range(10):
     #    print("i opt_conv.weight.grad = ", i, opt_conv.weight.grad.view(-1)[i].item())
 
-    opt_x_grad = x1.grad.to(torch.float)
+    if opt_has_physical_padding != False:
+        print("debug: Zeroing the rim of the x_grad tensor on the Python side")
+        nchw_shape = x1.grad.shape
+        opt_x_grad_zeroed_rim = torch.zeros_like(x1.grad)
+        print("debug: nchw shape = ", nchw_shape)
+        inH = nchw_shape[2] - input_hw_padding[0] - input_hw_padding[1]
+        inW = nchw_shape[3] - input_hw_padding[2] - input_hw_padding[3]
+        print("range = ", 'full', ' ', 'full', input_hw_padding[0], inH + input_hw_padding[0], input_hw_padding[2], inW + input_hw_padding[2])
+        opt_x_grad_zeroed_rim[:,:,input_hw_padding[0]:inH + input_hw_padding[0],input_hw_padding[2]:inW + input_hw_padding[2]] = x1.grad[:,:,input_hw_padding[0]:inH + input_hw_padding[0],input_hw_padding[2]:inW + input_hw_padding[2]]
+        opt_x_grad = opt_x_grad_zeroed_rim.to(torch.float)
+    else:
+        opt_x_grad = x1.grad.to(torch.float)
+    #opt_x_grad = x1.grad.to(torch.float)
+
     opt_weight_grad = opt_conv.weight.grad.to(torch.float)
-    opt_y_fp32 = y1.unblocked_tensor().to(torch.float)
+    #opt_y_fp32 = y1.unblocked_tensor().to(torch.float)
 
     """
     if opt_dtype == torch.bfloat16:
@@ -199,12 +298,20 @@ def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_
     ref_weight_grad = torch_conv.weight.grad.to(torch.float)
     ref_y_fp32 = y2.to(torch.float)
 
+    if opt_has_physical_padding != False:
+        ref_y_fp32 = torch.nn.functional.pad(ref_y_fp32, output_hw_padding, mode='constant', value=0.0)
+        ref_x_grad = torch.nn.functional.pad(ref_x_grad, input_hw_padding,  mode='constant', value=0.0)
+
+    shift = output_hw_padding[0] * (W + output_hw_padding[2] + output_hw_padding[3]) + output_hw_padding[2]
+    print("shift = ", shift)
     for i in range(10):
-        ind = i # + (output_hw_padding[0] * (W + output_hw_padding[2] + output_hw_padding[3]) + output_hw_padding[2]) - 5 if opt_padding != None else i
+        ind = i + shift - 5 if opt_has_physical_padding != False else i
         print("ind opt_y_fp32 ref_y_fp32 = ", ind, opt_y_fp32.view(-1)[ind].item(), ref_y_fp32.view(-1)[ind].item())
 
+    shift = (input_hw_padding[0] * (W + input_hw_padding[2] + input_hw_padding[3]) + input_hw_padding[2])
+    print("shift = ", shift)
     for i in range(10):
-        ind = i # + (output_hw_padding[0] * (W + output_hw_padding[2] + output_hw_padding[3]) + output_hw_padding[2]) - 5 if opt_padding != None else i
+        ind = i + shift - 5 if opt_has_physical_padding != False else i
         print("ind opt_x_grad ref_x_grad = ", ind, opt_x_grad.view(-1)[ind].item(), ref_x_grad.view(-1)[ind].item())
 
     """
@@ -379,7 +486,7 @@ def run_test_conv(N, H, W, inc, outc, K, stride, padding, dilation, groups, has_
         time_per_upd = time_per_iter #- time_per_fwd
         print("time per upd = ", time_per_upd)
 
-    exit()
+    #exit()
 
 def main():
     opt_dtype = torch.float if not args.use_bf16_opt else torch.bfloat16
@@ -391,7 +498,8 @@ def main():
     #with open("resnet50_conv_test_data_extended.data") as f:
     #with open("resnet50_conv_test_data_extended_new.data") as f:
     #with open("resnet50_conv_test_data_extended_new_28thr_reordered.data") as f:
-    with open("resnet50_conv_test_data_extended_new_28thr.data") as f:
+    #with open("resnet50_conv_test_data_extended_new_28thr.data") as f:
+    with open("resnet50_conv_test_data_for_bottleneck_28thr.data") as f:
         contents = f.readlines()
         for line in contents:
             #print("line")
