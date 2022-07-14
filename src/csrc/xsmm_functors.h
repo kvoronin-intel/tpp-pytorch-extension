a4f88474804c1c427e7e5751a7d73b716485f078
Dhiraj D Kalamkar <dhiraj.d.kalamkar@intel.com>
2022-07-13 22:17:00 -0700

    Move to latest libxsmm

--- src/csrc/xsmm_functors.h
+++ src/csrc/xsmm_functors.h
@@ -4055,9 +4055,8 @@ class EmbBagFwdTPP {
             XsmmDtype<Tout>(),
             LIBXSMM_DATATYPE_F32,
             (libxsmm_meltw_unary_flags)(
-                LIBXSMM_MELTW_FLAG_UNARY_REDUCE_XOR_ACC |
-                (sizeof(Tind) == 8 ? LIBXSMM_MELTW_FLAG_UNARY_IDX_SIZE_8BYTES
-                                   : LIBXSMM_MELTW_FLAG_UNARY_IDX_SIZE_4BYTES)),
+                sizeof(Tind) == 8 ? LIBXSMM_MELTW_FLAG_UNARY_IDX_SIZE_8BYTES
+                                  : LIBXSMM_MELTW_FLAG_UNARY_IDX_SIZE_4BYTES),
             LIBXSMM_MELTW_TYPE_UNARY_REDUCE_COLS_IDX_OP_ADD) {}
   void operator()(Tout* output, Tin* weight, Tind* input, int N) {
     unsigned long long _N = N;
