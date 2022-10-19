92d3ee980d5c058a1fe21908fcf68f095c5f8116
Mohanty, Ramanarayan <ramanarayan.mohanty@intel.com>
2022-10-19 02:00:14 -0700

    GAT updated

--- examples/gnn/gat/run_ss_gat_f32.sh
+++ examples/gnn/gat/run_ss_gat_f32.sh
@@ -15,10 +15,7 @@ for bsz in 1024 #10240
       echo "batch size = "$bsz
       mdir_ss=$1"_model_rank1_f32_optmlp_"$bsz"_dl_"$dl
       mkdir $mdir_ss
-      #./run_dist.sh -n $ranks -ppn 2 -f ~/hostfile -dl 4 ./run_f32_unidr.sh 4 $mdir |& tee $1"_log_"$ranks"_f32"
-      ./run_gat.sh 38 $dl $mdir_ss $bsz |& tee $mdir_ss/$1"_log_ss_f32_papers_icx_gat"
-      #./run_dist.sh -n $ranks -ppn 2  -dl $dl ./run_f32_undir_gat.sh 0 $dl $mdir_0c |& tee $mdir_0c/$1"_log_0c_"$ranks"_f32""_papers_icx_gat"
-    #done
+      ./run_gat_all.sh 38 $dl $mdir_ss $bsz |& tee $mdir_ss/$1"_log_ss_f32_papers_icx_gat"
   fi
 done
 
